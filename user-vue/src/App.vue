714a6366791fc063a081869ea3bece5d609313cf
lcq9 <3056703919@qq.com>
2023-05-16 08:18:51 +0800

    第一次提交

---
+++ user-vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <loginUser/>
+  </div>
+</template>
+
+<script>
+import loginUser from './components/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    loginUser
+  }
+}
+</script>
---
+++ user-vue/src/components/login.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="login">
+    <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px" class="demo-loginForm">
+    <el-form-item label="姓名">
+      <el-input v-model="loginForm.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="pass">
+      <el-input type="password" v-model="loginForm.pass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="checkPass">
+      <el-input type="password" v-model="loginForm.checkPass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="年龄" prop="age">
+      <el-input v-model.number="loginForm.age"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+      <el-button @click="resetForm('loginForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginUser',
+  data() {
+      var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.loginForm.checkPass !== '') {
+            this.$refs.loginForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.loginForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        loginForm: {
+          name: '',
+          pass: '',
+          checkPass: '',
+          age: ''
+        },
+        rules: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          age: [
+            { validator: checkAge, trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            console.log(this.loginForm.name)
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+}
+</script>
+
+<style scoped>
+.login{
+  width: 500px;
+  margin: 150px auto 0 auto;
+}
+</style>
